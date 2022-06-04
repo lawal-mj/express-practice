3c618f4af625e619bb3dc3241a90a5206934cf56
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-04 23:19:04 +0100

    nothing

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const app = express()
+app.set("view engine", "ejs")
 const data = require("./data")
 
-app.set("view engine", "ejs")
-
-
 app.get('/', function(req, res){
-    res.render('index', {name:"lawal"})
+    res.render('index', data[2])
 })
 
 
