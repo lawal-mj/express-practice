dcb0285ae7d2f2d072b9b91e422fac6617d99b36
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-04 23:34:06 +0100

    add tasks router

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express()
 app.set("view engine", "ejs")
 const data = require("./data")
 
+const taskRouter = require('./routes/tasks')
+app.use('/tasks', taskRouter)
+
 app.get('/', function(req, res){
     res.render('index', data[2])
 })
