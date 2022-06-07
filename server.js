c5a1be78bb9f623d47302296a928fa7c0a3fdc64
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-07 03:33:51 +0100

    remove use of imported data

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const data = require("./data")
 app.set("view engine", "ejs")
 
 const taskRouter = require('./routes/tasks')
@@ -13,12 +12,9 @@ app.use('/users', userRouter)
 
 // home route 
 app.get('/', function(req, res){
-    res.render('index', data[2])
+    res.render('index')
 })
 
 
 app.listen(3000)
 
-// route_path: '/user/:userId/book/:bookId'
-// actual_request_URL: '/user/546/book/6754'
-// req.params: {userId: '546', bookId: '6754'}
